12b519f7f274727e6f62a1bfc3a20e331f31762c
Nick Hikari <nicholas021205@gmail.com>
2025-03-15 07:57:32 +1100

    env file with the good stuff

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -3,19 +3,20 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { Firestore, getFirestore } from "@firebase/firestore";
 import { getAuth } from "@firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+require('dotenv').config();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAyXrFv7nsRwFk-ytk2xjR8Jp52eKtwRs8",
-  authDomain: "blueprint-unihack-app.firebaseapp.com",
-  projectId: "blueprint-unihack-app",
-  storageBucket: "blueprint-unihack-app.firebasestorage.app",
-  messagingSenderId: "104107251973",
-  appId: "1:104107251973:web:2b392c8a310acdbeb3bf6c",
-  measurementId: "G-6RLV04CCD0"
+  apiKey: process.env.REACT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_PUBLIC_FIREBASE_APP_ID,
+  measurmentId: process.env.REACT_PRIVATE_FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase
